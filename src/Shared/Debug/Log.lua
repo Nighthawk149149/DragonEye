7bbfad45e113b03309ce13b40693f81e83bc9d9a
Nicholas Stienz <nickstienz@gmail.com>
2022-12-17 19:59:32 -0600

    Better file struct

--- src/Shared/Debug/Log.lua
+++ src/Shared/Debug/Log.lua
@@ -1,22 +1,41 @@
 --[[
 Dragon Eye - A universal Roblox Anti-Cheat
 Copyright (C) 2022  Nicholas Stienz <nickstienz@gmail.com>
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ]]--
 --!strict
 
-local debug = require(game.ReplicatedStorage:WaitForChild("DragonEyeShared"):WaitForChild("debug"))
+local p, f = print, string.format
 
-debug.log(debug.logCode.LogClient, "Starting Dragon Eye Client...")
+--[[
+    Test/Extra Codes start at:  0
+    Server Codes Start at:      100
+    Client Codes Start at:      200
+    Shaded Codes Start at:      300
+]]--
+local codes = {
+    test = 0,
+
+    server = {
+        Log = 100,
+        Warn = 101,
+    }
+}
+
+local function write(code: number, file: string, message: string): ()
+    p(f("[Dragon Eye] (%s) Error: %d, Message: %s", file, code, message))
+end
+
+return {
+    write = write,
+    codes = codes,
+}
--- src/server/main.server.lua
+++ src/server/main.server.lua
@@ -1,22 +1,19 @@
 --[[
 Dragon Eye - A universal Roblox Anti-Cheat
 Copyright (C) 2022  Nicholas Stienz <nickstienz@gmail.com>
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ]]--
 --!strict
 
-local debug = require(game.ReplicatedStorage:WaitForChild("DragonEyeShared"):WaitForChild("debug"))
+local log = require(game.ReplicatedStorage.DragonEyeShared.Debug.Log)
 
-debug.log(debug.logCode.LogServer, "Starting Dragon Eye Server...")
+log.write(log.codes.server.Log, script:GetFullName(), "Starting...")
