7f346b5273862b2354d011be36c6fd02b3b1ea9d
Nick Stienz <nickstienz@gmail.com>
2022-12-30 17:27:48 -0600

    I quit programming
    Update README.md

--- src/Luau/Server/Main.server.lua
+++ src/Luau/Server/Main.server.lua
@@ -21,17 +21,29 @@ log.write(log.codes.server.log, script:GetFullName(), "Starting...")
 
 -- Check for Dragon Eye Version
 -- https://raw.githubusercontent.com/Nighthawk149149/DragonEye/master/VERSION.txt
-local currentVersion = game.ReplicatedStorage.DragonEyeShared.Settings.Version
-local serverVersion
+--[[
+    1. Get the current version from the local server
+    2. Send a GET request to the server to get the latest version and store it in a variable
+    3a. If the current version and the latest version do not match, log a warning
+    3b. If the current version and the latest version match, continue
+    3c. If the GET request fails due to HTTP requests not being enabled, log an error
+    3d. If the GET request fails for any other reason, log an error
+]]--
+if game.ReplicatedStorage.DragonEyeShared.Settings.CheckForUpdates == true then
+    local currentVersion = game.ReplicatedStorage.DragonEyeShared.Settings.Version
+    local serverVersion
 
-local httpService = game:GetService("HttpService")
-local success, err = pcall(function() serverVersion = httpService:GetAsync("https://raw.githubusercontent.com/Nighthawk149149/DragonEye/master/VERSION.txt") end)
-if success then
-    if currentVersion ~= serverVersion then
-        log.write(log.codes.server.warn, script:GetFullName(), "The local version and server version do not match! Please update the Anti-Cheat to the latest version.")
+    local httpService = game:GetService("HttpService")
+    local success, err = pcall(function() serverVersion = httpService:GetAsync("https://raw.githubusercontent.com/Nighthawk149149/DragonEye/master/VERSION.txt") end)
+    if success then
+        if currentVersion ~= serverVersion then
+            log.write(log.codes.server.warn, script:GetFullName(), "The local version and server version do not match! Please update the Anti-Cheat to the latest version.")
+        else
+            log.write(log.codes.server.log, script:GetFullName(), "Dragon Eye "..currentVersion .. " is running! (Latest Version)")
+        end
+    elseif err:lower():find("http requests are not enabled") then
+        log.write(log.codes.server.http_not_enabled, script:GetFullName(), err)
+    else
+        log.write(log.codes.server.http_fail, script:GetFullName(), err)
     end
-elseif err:lower():find("http requests are not enabled") then
-    log.write(log.codes.server.http_not_enabled, script:GetFullName(), err)
-else
-    log.write(log.codes.server.http_fail, script:GetFullName(), err)
 end
